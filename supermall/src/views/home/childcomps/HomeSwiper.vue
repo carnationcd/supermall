b0f5045516e7592d7046ca58607b86210f7ff2b1
Rain Zhang1 <Rain_Zhang1@jabil.com>
2021-12-03 09:56:16 +0800

    main page update

---
+++ supermall/src/views/home/childcomps/HomeSwiper.vue
@@ -0,0 +1,31 @@
+<template>
+  <swiper>
+    <swiper-item v-for="item in banners" :key="item.acm">
+      <a v-bind:href="item.link">
+        <img v-bind:src="item.image" alt="">
+      </a>
+    </swiper-item>
+  </swiper>
+</template>
+
+<script>
+  import {Swiper, SwiperItem} from 'components/common/swiper'
+  export default {
+    name: "HomeSwiper",
+    props:{
+      banners: {
+        type: Array,
+        default() {
+          return []
+        }
+      }
+    },
+    components: {
+      Swiper,
+      SwiperItem
+    }
+  }
+</script>
+
+<style>
+</style>
---
+++ supermall/src/views/home/childcomps/RecommendView.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="recommend">
+    <div v-for="item in recommends" :key="item.acm" class="recommend-item">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+        <div>{{ item.title }}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'RecommendView',
+    props: {
+      recommends: {
+        type: Array,
+        default() {
+          return []
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .recommend {
+    display: flex;
+    text-align: center;
+    font-size: 12px;
+    padding: 5px 0px;
+    border-bottom: 8px solid #EEEEEE;
+    width: 100%;
+  }
+  .recommend-item {
+    flex: 1;
+  }
+  .recommend-item img {
+    width: 70px;
+    height: 70px;
+    margin-bottom: 10px;
+  }
+</style>
